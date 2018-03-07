e75d1f754afc40b3acecf35c71086bd34927e734
Harsha Bommana <harshablast@gmail.com>
2018-03-07 08:03:28 +0530

    Trying AffinityPropagation clustering

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import gensim
 from minisom import MiniSom
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
-
-
+from sklearn.cluster import KMeans,AffinityPropagation
+import numpy as np
 
 model = gensim.models.KeyedVectors.load_word2vec_format('glove.twitter.27B.200d.Word2Vecformat.txt')
 
+
 test_word = 'tie'
 
-
-nearest_words = model.most_similar(positive = [test_word], topn = 100)
+nearest_words = model.most_similar(positive = [test_word], topn = 1000)
 nearest_vectors = []
 
 for i in nearest_words:
@@ -24,21 +24,28 @@ som.train_random(nearest_vectors, 100) # trains the SOM with 100 iterations
 print("...ready!")
 """
 
-from sklearn.cluster import KMeans
-import numpy as np
 
 nearest_vectors = np.array(nearest_vectors)
 
 kmeans = KMeans(n_clusters = 5, random_state = 0).fit(nearest_vectors)
 results = kmeans.labels_
 
+affinity = AffinityPropagation()
+affinity.fit(nearest_vectors)
+results_affinity = affinity.labels_
+
 
 groups = [[],[],[],[],[]]
+groups_affinity = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
 
 for i in range(len(results)):
     groups[results[i]].append(nearest_words[i])
     
-    
+for i in range(len(results_affinity)):
+    print(i)
+    groups_affinity[results_affinity[i]].append(nearest_words[i])
+    print(results_affinity[i])
+    print(groups_affinity)
 
 pca = PCA(n_components = 2)
 pca.fit(nearest_vectors)
